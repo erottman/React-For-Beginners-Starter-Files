25e92a2fca109b3a260e2d10ab67565d00e49a31
Erica Rottman <erica7650@gmail.com>
2017-08-10 07:01:05 -0700

    finished state and jsx modules

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Fish extends Component {
+  render () {
+    const { details } = this.props;
+    return (
+    <li className='menu-fish'>
+      <img src={details.image} alt={details.name} />
+      <h3 className="fish-name">
+        {details.name}
+        <span className="price">{details.price}</span>
+      </h3>
+      <p>{details.desc}</p>
+      <button>Add to Order</button>
+    </li>
+    )
+  }
+}
+
+export default Fish;
---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class NotFound extends Component {
+  render () {
+    return (
+    <h2>Not Found!11111!</h2>
+    )
+  }
+}
+
+export default NotFound;
